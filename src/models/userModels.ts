2053c682982c238271240299038ad99293014397
markfranca_ <mvfranca2004@gmail.com>
2026-02-03 14:14:46 -0300

    refactor: trocando o nome das variaveis

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -2,26 +2,26 @@ import { PrismaClient } from "../generated/prisma/client";
 import prisma from "../lib/prisma";
 
 
-export const getAllUsers = async () => {
+export const getAllUsersModel = async () => {
   const users = await prisma.user.findMany();
   return users;
 }
 
-export const getUserById = async (userId: number) => {
+export const getUserByIdModel = async (userId: number) => {
     const user = await prisma.user.findUnique({
         where : { id: userId },
     });
     return user;
 }
 
-export const createUser = async (userData: any) => {
+export const createUserModel = async (userData: any) => {
     const newUser = await prisma.user.create({
         data: userData,
     });
     return newUser;
 }
 
-export const updateUser = async (userId: number, userData: any) => {
+export const updateUserModel = async (userId: number, userData: any) => {
   const updatedUser = await prisma.user.update({
       where: { id: userId },
       data: userData,
@@ -29,7 +29,7 @@ export const updateUser = async (userId: number, userData: any) => {
   return updatedUser;
 }
 
-export const deleteUser = async (userId: number) => {
+export const deleteUserModel = async (userId: number) => {
     const deletedUser = await prisma.user.delete({
         where: {id: userId },
     });
