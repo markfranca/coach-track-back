044b184212855ca6b0fa6307349654ac8d211ee5
markfranca_ <mvfranca2004@gmail.com>
2026-02-03 13:54:53 -0300

    feat: criando base para models

---
+++ src/models/classModels.ts
@@ -0,0 +1,21 @@
+import prisma from "../lib/prisma";
+
+export const getAllClasses = async () => {
+    // Implementation for fetching all classes
+}
+
+export const getClassById = async (classId: number) => {
+    // Implementation for fetching a class by ID
+}
+
+export const createClass = async (classData: any) => {
+    // Implementation for creating a new class
+} 
+
+export const updateClass = async (classId: number, classData: any) => {
+    // Implementation for updating a class
+}
+
+export const deleteClass = async (classId: number) => {
+    // Implementation for deleting a class
+}
---
+++ src/models/teacherModels.ts
@@ -0,0 +1,22 @@
+import prisma from "../lib/prisma";
+
+
+export const getAllTeachers = async () => {
+    // Implementation for fetching all teachers
+}
+
+export const getTeacherById = async (teacherId: number) => {
+    // Implementation for fetching a teacher by ID
+}
+
+export const createTeacher = async (teacherData: any) => {
+    // Implementation for creating a new teacher
+}   
+
+export const updateTeacher = async (teacherId: number, teacherData: any) => {
+    // Implementation for updating a teacher
+}
+
+export const deleteTeacher = async (teacherId: number) => {
+    // Implementation for deleting a teacher
+}
